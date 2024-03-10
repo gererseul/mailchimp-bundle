234f8e0d1b23b84e141c367acc3038429ad0dbf4
m.tlili <tlili.mokhtar@gmail.com>
2024-03-10 18:00:13 +0100

    Fix compatible declaration getContainerExtension

--- src/WelpMailchimpBundle.php
+++ src/WelpMailchimpBundle.php
@@ -3,11 +3,12 @@
 namespace Welp\MailchimpBundle;
 
 use Symfony\Component\HttpKernel\Bundle\Bundle;
+use Symfony\Component\DependencyInjection\Extension\ExtensionInterface;
 use Welp\MailchimpBundle\DependencyInjection\WelpMailchimpExtension;
 
 class WelpMailchimpBundle extends Bundle
 {
-    public function getContainerExtension()
+    public function getContainerExtension(): ?ExtensionInterface
     {
         return new WelpMailchimpExtension();
     }
